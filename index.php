c4b1dd93a47efde8461e6e57b19b635ef7e2b37a
Sayedjoya <45958843+Sayedjoya@users.noreply.github.com>
2022-04-01 00:17:09 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+
+<meta name="propeller" content="10020ec6e0fe37d1332ec9cb369fc69e"><center>
+
+
+<!DOCTYPE html>
+<html style="height:100%">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+<title> wh973381
+</title></head>
+<body style="color: #5f1a37; margin:0;font: normal 14px/20px Arial, Helvetica, sans-serif; height:100%; background-color: #fff;">
+<div style="height:auto; min-height:100%; ">     <div style="text-align: center; width:800px; margin-left: -400px; position:absolute; top: 30%; left:50%;">
+
+<center><img src="https://i.pinimg.com/originals/54/c4/3b/54c43b3256908af8f552c3bae869e6af.gif" alt="Dancing GIFs | Tenor" jsname="HiaYvf" jsaction="load:XAeZkd;" class="n3VNCb" data-noaft="1" style="width: 180px; margin: 0px;">
+
+<h1 style="margin-top:20px;font-size: 30px;">wh973381
+</h1>
+<p>Plz Join Telegram Grup</p></center>
+  <div><a href="/hindi.php" target="_blank"><b><h1>Hindi Live</h1></b></a></div>
+<div><a href="/en.php" target="_blank"><b><h1>English Live</h1></b></a></div>
+  
+</div></div>
+<center><a style="display:block;font-size:16px;font-weight:500;text-align:center;border-radius:8px;padding:5px;background:#389ce9;text-decoration:none;color:#fff;" href="https://xn--r1a.click/wh973381" target="_blank"><svg style="width:30px;height:20px;vertical-align:middle;margin:0px 5px;" viewBox="0 0 21 18"><g fill="none"><path fill="#ffffff" d="M0.554,7.092 L19.117,0.078 C19.737,-0.156 20.429,0.156 20.663,0.776 C20.745,0.994 20.763,1.23 20.713,1.457 L17.513,16.059 C17.351,16.799 16.62,17.268 15.88,17.105 C15.696,17.065 15.523,16.987 15.37,16.877 L8.997,12.271 C8.614,11.994 8.527,11.458 8.805,11.074 C8.835,11.033 8.869,10.994 8.905,10.958 L15.458,4.661 C15.594,4.53 15.598,4.313 15.467,4.176 C15.354,4.059 15.174,4.037 15.036,4.125 L6.104,9.795 C5.575,10.131 4.922,10.207 4.329,10.002 L0.577,8.704 C0.13,8.55 -0.107,8.061 0.047,7.614 C0.131,7.374 0.316,7.182 0.554,7.092 Z"></path></g></svg>Join Telegram</a>
+
+</center>
+
+
+
+
+
+
+
