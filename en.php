e92937969593d34e13d135e54b17fc9bea774d10
Sayedjoya <45958843+Sayedjoya@users.noreply.github.com>
2022-04-01 00:18:36 +0530

    Create en.php

---
+++ en.php
@@ -0,0 +1,63 @@
+<meta name="propeller" content="10020ec6e0fe37d1332ec9cb369fc69e">
+
+<script>(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://iclickcdn.com/tag.min.js',4987922,document.body||document.documentElement)</script>
+<script>(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://iclickcdn.com/tag.min.js',4987926,document.body||document.documentElement)</script><script type="text/javascript" src="https://cdn.jsdelivr.net/clappr/latest/clappr.min.js"></script>
+ <div class="theoplayer-container video-js theoplayer-skin vjs-16-9" data-src="https://diokz91u21fy1.cloudfront.net/iplch1/2022/master.m3u8" data-poster="https://i.ibb.co/j3y22wv/images-2022-03-31-T205713-252.jpg"></div>
+<script>
+  var element, player;
+
+  function createPlayer() {
+    element = document.querySelector('.theoplayer-container');
+    player = new THEOplayer.Player(element, {
+      libraryLocation: 'https://cdn.myth.theoplayer.com/55d71743-4924-4d1b-8440-78f9cd103879/'
+    });
+    player.preload = "auto";
+  }
+
+  function setSrc(src, poster) {
+    if (!player) {
+      createPlayer();
+    }
+    player.source = {
+      "sources": {
+        "src": src
+      },
+      "poster": poster
+    }
+  }
+  window.addEventListener('theoplayerready', function() {
+    if (!player) {
+      createPlayer();
+    }
+  })
+  document.addEventListener("DOMContentLoaded", function() {
+    var players = [].slice.call(document.querySelectorAll(".theoplayer-container"));
+    if ("IntersectionObserver" in window) {
+      var playerObserver = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(function(container) {
+          if (container.isIntersecting) {
+            var src = container.target.dataset.src
+            var poster = container.target.dataset.poster;
+            if (!window.THEOplayer) {
+              window.addEventListener('theoplayerready',
+                function() {
+                  setSrc(src, poster);
+                })
+            } else {
+              setSrc(src, poster);
+            }
+            playerObserver.unobserve(container.target);
+          }
+        });
+      });
+      players.forEach(function(playerElement) {
+        playerObserver.observe(playerElement);
+      });
+    }
+  });
+</script>
+<script async src="https://cdn.myth.theoplayer.com/55d71743-4924-4d1b-8440-78f9cd103879/THEOplayer.js"></script>
+<link rel="stylesheet" type="text/css" href='https://cdn.myth.theoplayer.com/55d71743-4924-4d1b-8440-78f9cd103879/ui.css'>
+
+<script>(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://iclickcdn.com/tag.min.js',4987926,document.body||document.documentElement)</script>
+
